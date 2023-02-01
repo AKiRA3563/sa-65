11bce73e1defa7cb8cd327171e848568ced928b6
AKiRA3563 <akiraphat_j63@hotmail.com>
2023-02-01 08:47:29 +0700

    update last sa-65

--- backend/entity/borrowlist.go
+++ backend/entity/borrowlist.go
@@ -9,71 +9,71 @@ import (
 type Employee struct {
 	gorm.Model
 
-	FirstName	string
-	LastName	string
-	Email		string	`gorm:"uniqueIndex"`
-	Password	string
-	
-	Equipment 	[]Equipment		`gorm:"foreignKey:EmployeeID"`
-	BorrowList	[]BorrowList	`gorm:"foreignKey:EmployeeID"`
+	FirstName string
+	LastName  string
+	Email     string `gorm:"uniqueIndex"`
+	Password  string
+
+	Equipment  []Equipment  `gorm:"foreignKey:EmployeeID"`
+	BorrowList []BorrowList `gorm:"foreignKey:EmployeeID"`
 }
 
 type User struct {
 	gorm.Model
 
 	//PrefixID	*uint
-	Prefix		string	//Prefix `gorm:"references:ID"`
+	Prefix string
 
-	FirstName	string
-	LastName	string
-	Email		string	`gorm:"uniqueIndex"`
-	Password	string
-	Address		string
-	Birthday	time.Time
+	FirstName string
+	LastName  string
+	Email     string `gorm:"uniqueIndex"`
+	Password  string
+	Address   string
+	Birthday  time.Time
 
 	//GenderID	*uint
-	Gender		string	//Gender `gorm:"references:ID"`
+	Gender string
 
-	Personal	string	`gorm:"uniqueIndex"`
-	Mobile		string	`gorm:"uniqueIndex"`
-	
-	ProvinceID	int
-	//Province	Province	`gorm:"references:ID"`
+	Personal string `gorm:"uniqueIndex"`
+	Mobile   string `gorm:"uniqueIndex"`
 
-	BorrowList []BorrowList	`gorm:"foreignKey:MemberID"`
+	ProvinceID int
+	//Province	Province
+
+	BorrowList []BorrowList `gorm:"foreignKey:MemberID"`
 }
 
 type Equipment struct {
 	gorm.Model
 
-	Name			string
-	Amount			int
-	Time	 		time.Time
+	Name   string
+	Amount int
+	Time   time.Time
 
-	CategoryID		int
-	// Category		Catagory	`gorm:"references:ID"`
+	CategoryID int
+	// Category	CatagoryID
 
-	UnitID			int
-	// Unit			Unit		`gorm:"references:ID"`
+	UnitID int
+	// Unit	Unit
 
-	EmployeeID		*uint
-	Employee		Employee	`gorm:"references:ID"`
+	EmployeeID *uint
+	Employee   Employee
 
-	BorrowList []BorrowList		`gorm:"foreignKey:EquipmentID"`
+	BorrowList []BorrowList `gorm:"foreignKey:EquipmentID"`
 }
 
-type BorrowList	struct {
+type BorrowList struct {
 	gorm.Model
 
-	BorrowTime		time.Time
-	Amount			int
+	BorrowTime time.Time
+	Amount     int
 
-	EquipmentID 	*uint		
-	Equipment		Equipment	`gorm:"references:ID"`
+	EquipmentID *uint
+	Equipment   Equipment `gorm:"references:ID"`
 
-	MemberID		*uint
-	Member			User		`gorm:"references:ID"`
+	MemberID *uint
+	Member   User `gorm:"references:ID"`
 
-	EmployeeID		*uint
-	Employee		Employee	`gorm:"references:ID"`
+	EmployeeID *uint
+	Employee   Employee `gorm:"references:ID"`
 }
--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -28,9 +28,9 @@ func SetupDatabase() {
 
 	// Migarate the schema
 	database.AutoMigrate(
-		&Equipment{}, 
-		&User{}, 
-		&Employee{}, 
+		&Equipment{},
+		&User{},
+		&Employee{},
 		&BorrowList{},
 	)
 
@@ -40,47 +40,47 @@ func SetupDatabase() {
 
 	//setup User
 	User1 := User{
-		Prefix:  	"Ms.",
-		FirstName: "Namjai",
-		LastName:  "Meedee",
+		Prefix:     "Ms.",
+		FirstName:  "Namjai",
+		LastName:   "Meedee",
 		Email:      "namn@gmail.com",
 		Password:   string(password),
 		Address:    "12 Moo 6 ...",
 		Birthday:   time.Now(),
-		Gender:  	"Female",
+		Gender:     "Female",
 		Personal:   "1234567890098",
 		Mobile:     "0812345678",
-		ProvinceID:   23,
+		ProvinceID: 23,
 	}
 	db.Model(&User{}).Create(&User1)
 
 	User2 := User{
-		Prefix:  	"Mr.",
-		FirstName: "Terdsak",
-		LastName:  "Srasai",
+		Prefix:     "Mr.",
+		FirstName:  "Terdsak",
+		LastName:   "Srasai",
 		Email:      "tesra@gmail.com",
 		Password:   string(password),
 		Address:    "122 Mueng ...",
 		Birthday:   time.Now(),
-		Gender:  	"Male",
+		Gender:     "Male",
 		Personal:   "5355201720164",
 		Mobile:     "0782256613",
-		ProvinceID:   70,
+		ProvinceID: 70,
 	}
 	db.Model(&User{}).Create(&User2)
 
 	User3 := User{
-		Prefix:  	"Mr.",
-		FirstName: "Arnon",
-		LastName:  "Borriboon",
+		Prefix:     "Mr.",
+		FirstName:  "Arnon",
+		LastName:   "Borriboon",
 		Email:      "Arnoo@gmail.com",
 		Password:   string(password),
 		Address:    "11 Moo 5 ...",
 		Birthday:   time.Now(),
-		Gender:  	"Male",
+		Gender:     "Male",
 		Personal:   "1234567890123",
 		Mobile:     "0812345678",
-		ProvinceID:   23,
+		ProvinceID: 23,
 	}
 	db.Model(&User{}).Create(&User3)
 
@@ -88,8 +88,8 @@ func SetupDatabase() {
 		//EmployeeID:  599323,
 		FirstName: "Manee",
 		LastName:  "Srisuruk",
-		Email:      "namee@gmail.com",
-		Password:   string(password),
+		Email:     "namee@gmail.com",
+		Password:  string(password),
 	}
 	db.Model(&Employee{}).Create(&Employee1)
 
@@ -97,8 +97,8 @@ func SetupDatabase() {
 		//EmployeeID:  599323,
 		FirstName: "Suree",
 		LastName:  "Junsri",
-		Email:      "reee@gmail.com",
-		Password:   string(password),
+		Email:     "reee@gmail.com",
+		Password:  string(password),
 	}
 	db.Model(&Employee{}).Create(&Employee2)
 
@@ -106,42 +106,40 @@ func SetupDatabase() {
 		//EmployeeID:  599323,
 		FirstName: "Rakmak",
 		LastName:  "Teesoot",
-		Email:      "love@gmail.com",
-		Password:   string(password),
+		Email:     "love@gmail.com",
+		Password:  string(password),
 	}
 	db.Model(&Employee{}).Create(&Employee3)
 
 	Equipment1 := Equipment{
-		Name: 			"Projector",
-		Amount: 		15,
-		Time: 			time.Now(),
-		CategoryID: 	1,
-		UnitID: 		1,
-		Employee: 		Employee1,
+		Name:       "Projector",
+		Amount:     15,
+		Time:       time.Now(),
+		CategoryID: 1,
+		UnitID:     1,
+		Employee:   Employee1,
 	}
 	db.Model(&Equipment{}).Create(&Equipment1)
 
 	Equipment2 := Equipment{
-		Name: 			"Whiteboard Pen",
-		Amount: 		23,
-		Time: 			time.Now(),
-		CategoryID: 	2,
-		UnitID: 		2,
-		Employee: 		Employee2,
+		Name:       "Whiteboard Pen",
+		Amount:     23,
+		Time:       time.Now(),
+		CategoryID: 2,
+		UnitID:     2,
+		Employee:   Employee2,
 	}
 	db.Model(&Equipment{}).Create(&Equipment2)
 
 	Equipment3 := Equipment{
-		Name: 			"Chairs",
-		Amount: 		10,
-		Time: 			time.Now(),
-		CategoryID: 	3,
-		UnitID: 		3,
-		Employee: 		Employee2,
+		Name:       "Chairs",
+		Amount:     10,
+		Time:       time.Now(),
+		CategoryID: 3,
+		UnitID:     3,
+		Employee:   Employee2,
 	}
 	db.Model(&Equipment{}).Create(&Equipment3)
-
-
 
 	//setup
 	// BorrowList1 := BorrowList{
